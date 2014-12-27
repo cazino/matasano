94bb45089a6367d561df6388ac0a034e3827c077
Emmanuel Cazenave <contact@emcaz.fr>
2014-12-27 22:45:54 +0100

    Xor

--- matasano/__init__.py
+++ matasano/__init__.py
@@ -4,3 +4,8 @@ import base64
 def hextobase64(hex_str):
     bytes_data = bytes.fromhex(hex_str)
     return base64.b64encode(bytes_data).decode()
+
+
+def xor(hex1, hex2):
+    result = int(hex1, 16) ^ int(hex2, 16)
+    return '{:x}'.format(result)
--- matasano/tests.py
+++ matasano/tests.py
@@ -9,3 +9,13 @@ class HexToBase64(unittest.TestCase):
         base64_str = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
         result = hextobase64(hex_str)
         self.assertEquals(base64_str, result)
+
+
+class Xor(unittest.TestCase):
+
+    def test(self):
+        from matasano import xor
+        hex1 = '1c0111001f010100061a024b53535009181c'
+        hex2 = '686974207468652062756c6c277320657965'
+        result = '746865206b696420646f6e277420706c6179'
+        self.assertEquals(result, xor(hex1, hex2))
