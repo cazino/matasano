acb9113a991037b3069fac828be9f7d66038daf5
Emmanuel Cazenave <contact@emcaz.fr>
2014-12-28 22:34:00 +0100

    Mess

--- matasano/__init__.py
+++ matasano/__init__.py
@@ -1,5 +1,9 @@
 import base64
 import binascii
+from collections import defaultdict
+import difflib
+import operator
+import string
 
 
 def hextobase64(hex_str):
@@ -20,3 +24,38 @@ def xor(hex1, hex2):
             elem2 = next(iterb2)
         result.append(elem1 ^ elem2)
     return binascii.hexlify(result).decode()
+
+
+def score(text):
+    """Scores plain english text  with
+    "ETAOIN SHRDLU" occurences
+    """
+
+    ref = "ETAOINSHRDLU"
+    exclude = " "
+    occurences = defaultdict(int)
+    for char in text:
+        if char not in exclude:
+            occurences[char] += 1
+    sorted_occ = sorted(occurences.items(),
+                        key=operator.itemgetter(1),
+                        reverse=True)
+    sorted_char = [x.capitalize() for (x, y) in sorted_occ]
+    return difflib.SequenceMatcher(a=ref, b=sorted_char).ratio()
+
+
+def decrypt(hexphrase):
+    """ hex is hex encoded string
+    hex is supposed to have been xor with a single character.
+    Return the most likely unencoded original string
+    """
+    results = defaultdict(float)
+    for char in string.printable:
+        char_bytes = bytes(char, 'ascii')
+        phrase = xor(hexphrase, binascii.hexlify(char_bytes).decode())\
+            .decode()
+        results[phrase] = score(phrase)
+    sorted_results = sorted(results.items(),
+                            key=operator.itemgetter(1),
+                            reverse=True)
+    return sorted_results[0][0]
--- matasano/tests.py
+++ matasano/tests.py
@@ -5,8 +5,10 @@ class HexToBase64(unittest.TestCase):
 
     def test(self):
         from matasano import hextobase64
-        hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-        base64_str = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+        hex_str = ('49276d206b696c6c696e6720796f757220627261696'
+                   'e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+        base64_str = ('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtl'
+                      'IGEgcG9pc29ub3VzIG11c2hyb29t')
         result = hextobase64(hex_str)
         self.assertEquals(base64_str, result)
 
@@ -26,3 +28,18 @@ class Xor(unittest.TestCase):
         hex2 = 'ff'    # 1111
         result = 'ffff'  # 11111111
         self.assertEquals(result, xor(hex1, hex2))
+
+
+class ScoreTest(unittest.TestCase):
+
+    def test_full_score(self):
+        from matasano import score
+        text = ('EEEEEEEEEEEEETTTTTTTTTTTT'
+                'AAAAAAAAAAAOOOOOOOOOOIIIIIIIII'
+                'NNNNNNNNSSSSSSSHHHHHHRRRRRDDDDLLUU')
+        self.assertEquals(1, score(text))
+
+    def test_null_score(self):
+        from matasano import score
+        text = 'x'
+        self.assertEquals(0, score(text))
