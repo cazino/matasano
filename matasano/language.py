b5db25982de4dfba512eac98564522d8842c557f
Emmanuel Cazenave <contact@emcaz.fr>
2014-12-30 22:34:08 +0100

    Refactoring again

--- matasano/language.py
+++ matasano/language.py
@@ -4,18 +4,30 @@ import operator
 
 class EnglishScorer(object):
     """Scores a text so that
-    an english text get a higher score.
+    an english text get a low score.
+    A text in another language or in a non langugage
+    should get a higher score.
     """
 
-    #def __init__(self, text):
-    #    self.text = text
+    def _distance(self, chars):
+        ref = "ETAOINSHRDLU"
+        distance = 0
+        for char in chars:
+            try:
+                tmp_dist = abs(chars.index(char) -
+                               ref.index(char))
+            except ValueError:
+                tmp_dist = 12
+            distance += tmp_dist
+        if chars:
+            return distance / (12 * len(chars))
+        return 1
 
     def score(self, text):
         """Scores plain english text  with
         "ETAOIN SHRDLU" occurences
         """
         text = text.upper()
-        ref = "ETAOINSHRDLU"
         occurences = defaultdict(int)
         for char in text:
             occurences[char] += 1
@@ -23,15 +35,4 @@ class EnglishScorer(object):
                             key=operator.itemgetter(1),
                             reverse=True)
         sorted_char = [x for (x, y) in sorted_occ if x != " "]
-        distance = 0
-        restricted_sorted_char = sorted_char[:12]
-        for char in restricted_sorted_char:
-            try:
-                tmp_dist = abs(restricted_sorted_char.index(char) -
-                               ref.index(char))
-            except ValueError:
-                tmp_dist = 12
-            distance += tmp_dist
-        if restricted_sorted_char:
-            return distance / (12 * len(restricted_sorted_char))
-        return 1
+        return self._distance(sorted_char[:12])
